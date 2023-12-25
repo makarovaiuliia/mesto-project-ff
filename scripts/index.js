45592a3adadd561beb0cd86ecc1bfad7c30b4eee
Iuliia Makarova <iuliia.makarova.au@gmail.com>
2023-12-25 17:16:23 +0100

    corrections after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,29 @@
 const cardContainer = document.querySelector(".places__list");
 
-initialCards.forEach((item) => {
-  addCard(item.name, item.link);
-});
-
-function addCard(place, imageLink) {
+function createCard(item, deleteCard) {
   const template = document.getElementById("card-template").content;
   const cardElement = template.querySelector(".card").cloneNode(true);
+
   const title = cardElement.querySelector(".card__title");
   const image = cardElement.querySelector(".card__image");
-  image.src = imageLink;
-  title.textContent = place;
+  image.src = item.link;
+  image.alt = `Фотография места в России: ${item.name}`;
+  title.textContent = item.name;
 
   const deleteBtn = cardElement.querySelector(".card__delete-button");
   deleteBtn.addEventListener("click", deleteCard);
-  cardContainer.append(cardElement);
+
+  return cardElement;
+}
+
+function addCard(item) {
+  const card = createCard(item, deleteCard);
+  cardContainer.append(card);
 }
 
 function deleteCard(event) {
   const item = event.target.closest(".card");
   item.remove();
 }
+
+initialCards.forEach(addCard);
